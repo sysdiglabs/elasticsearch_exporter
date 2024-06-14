dbb446099c12243468fe2f09dd81fa84436ae3a4
Jaime Yera Hidalgo <106755265+jaimeyh@users.noreply.github.com>
2024-06-14 11:47:24 +0200

    Revert "Refactor snapshots collector (#789)"
    This reverts commit 34423407fa453a35c3899b7d906dafd568efe359.

--- collector/snapshots_test.go
+++ collector/snapshots_test.go
@@ -209,12 +209,15 @@ func TestSnapshots(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			c, err := NewSnapshots(log.NewNopLogger(), u, http.DefaultClient)
-			if err != nil {
-				t.Fatal(err)
-			}
+			s := NewSnapshots(log.NewNopLogger(), http.DefaultClient, u)
 
-			if err := testutil.CollectAndCompare(wrapCollector{c}, strings.NewReader(tt.want)); err != nil {
+			// TODO: Convert to collector interface
+			// c, err := NewSnapshots(log.NewNopLogger(), u, http.DefaultClient)
+			// if err != nil {
+			// 	t.Fatal(err)
+			// }
+
+			if err := testutil.CollectAndCompare(s, strings.NewReader(tt.want)); err != nil {
 				t.Fatal(err)
 			}
 		})
